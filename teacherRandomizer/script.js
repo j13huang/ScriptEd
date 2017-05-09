0337ad01ea3beb79024b2367c34d74e04a4a3334
John Huang <j13huang@gmail.com>
2017-05-08 20:58:59 -0700

    working code

--- teacherRandomizer/script.js
+++ teacherRandomizer/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  var scriptEducators = ["Ariel", "Emily", "John", "Luna", "Razzi"];
+
+  $("button").click(function() {
+      var randomIndex = Math.floor(Math.random() * scriptEducators.length);
+      $("#response").html(scriptEducators[randomIndex] + ", I need some help!");
+  });
 });
 
